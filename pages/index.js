f9d6342b8d43091312e510fada072727d19f8c07
Rushank Shah <rushankshah65@gmail.com>
2020-12-10 22:32:59 +0530

    Animations added to home page

--- pages/index.js
+++ pages/index.js
@@ -3,6 +3,7 @@ import { Row, Col, Container } from 'reactstrap'
 import Typed from 'react-typed'
 import { useEffect, useState } from 'react'
 import Link from 'next/link'
+import { Flip, Zoom } from 'react-awesome-reveal'
 
 export default function Home() {
 
@@ -36,41 +37,45 @@ export default function Home() {
         <Container>
           <Row>
             <Col md="6">
-              <div className="hero-section">
-                <div className={`flipper ${isFlipping ? 'isFlipping' : ''}`}>
-                  <div className="front">
-                    <div className="hero-section-content">
-                      <h2> Hello World! </h2>
-                      <div className="hero-section-content-intro">
-                        Have a look at my portfolio!
-                    </div>
-                    </div>
-                    <img className="image" src="/static/images/section-1.png" />
-                    <div className="shadow-custom">
-                      <div className="shadow-inner"> </div>
-                    </div>
+              <Flip>
+                <div className="hero-section">
+                  <div className={`flipper ${isFlipping ? 'isFlipping' : ''}`}>
+                    <div className="front">
+                      <div className="hero-section-content">
+                        <h2> Hello World! </h2>
+                        <div className="hero-section-content-intro">
+                          Have a look at my portfolio!
                   </div>
-                  <div className="back">
-                    <div className="hero-section-content">
-                      <h2> An Enthusiast Developer </h2>
-                      <div className="hero-section-content-intro">
-                        Open for collaborations
+                      </div>
+                      <img className="image" src="/static/images/section-1.png" />
+                      <div className="shadow-custom">
+                        <div className="shadow-inner"> </div>
+                      </div>
                     </div>
-                    </div>
-                    <img className="image" src="/static/images/section-2.png" />
-                    <div className="shadow-custom shadow-custom-2">
-                      <div className="shadow-inner"> </div>
+                    <div className="back">
+                      <div className="hero-section-content">
+                        <h2> An Enthusiast Developer </h2>
+                        <div className="hero-section-content-intro">
+                          Open for collaborations
+                  </div>
+                      </div>
+                      <img className="image" src="/static/images/section-2.png" />
+                      <div className="shadow-custom shadow-custom-2">
+                        <div className="shadow-inner"> </div>
+                      </div>
                     </div>
                   </div>
                 </div>
-              </div>
+              </Flip>
             </Col>
             <Col md="6" className="hero-welcome-wrapper">
-              <div className="hero-welcome-text">
-                <h1 style={{ textAlign: 'center' }}>
-                  नमस्ते! I am <strong>Rushank Shah</strong>.
-                </h1>
-              </div>
+              <Zoom direction='left'>
+                <div className="hero-welcome-text">
+                  <h1 style={{ textAlign: 'center' }}>
+                    नमस्ते! I am <strong>Rushank Shah</strong>.
+                  </h1>
+                </div>
+              </Zoom>
               <div style={{ textAlign: 'center' }}>
                 <Typed
                   strings={
@@ -82,11 +87,13 @@ export default function Home() {
                   className='self-typed'
                 />
               </div>
-              <div className="hero-welcome-bio">
-                <h1 style={{ textAlign: 'center' }}>
-                  Happy to have you on board🥳.<br></br> You can read more about me <Link href='/about'><a style={{ textDecoration: 'none' }} className='here'><i>here</i></a></Link>.
-                </h1>
-              </div>
+              <Zoom direction='right'>
+                <div className="hero-welcome-bio">
+                  <h1 style={{ textAlign: 'center' }}>
+                    Happy to have you on board🥳.<br></br> You can read more about me <Link href='/about'><a style={{ textDecoration: 'none' }} className='here'><i>here</i></a></Link>.
+                  </h1>
+                </div>
+              </Zoom>
             </Col>
           </Row>
         </Container>
