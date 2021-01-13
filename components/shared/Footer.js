d5869e5de81225b07bffa43cb5e775429fde1157
Rushank Shah <rushankshah65@gmail.com>
2021-01-13 17:35:38 +0530

    Footer changes

--- components/shared/Footer.js
+++ components/shared/Footer.js
@@ -1,5 +1,5 @@
-import { Slide, Zoom } from "react-awesome-reveal";
-import { Col, Row } from "reactstrap";
+import { Bounce, Zoom } from "react-awesome-reveal";
+import { Row } from "reactstrap";
 
 export default function Footer() {
     return (
@@ -10,63 +10,40 @@ export default function Footer() {
                         Let's connect!
                     </h5>
                 </Zoom>
-                <Slide direction='left'>
-                    <h5 style={{ textAlign: 'center', color: 'black' }}>
-                        <a target='_blank' style={{ color: 'black', textDecoration: 'none' }} href='mailto:rushankshah65@gmail.com'>
-                            <i className="lni lni-envelope lni-lg lni-25 social-media">rushankshah65@gmail.com</i>
-                        </a>
-                    </h5>
-                </Slide>
-                <Slide direction='right'>
-                    <Row className='mt-3' style={{ textAlign: 'center', color: 'black' }}>
-                        <Col md='12'>
-                            <Row>
-                                <Col md='2'>
-                                    <div>
-                                        <a target='_blank' style={{ color: 'black', textDecoration: 'none' }} href='https://www.linkedin.com/in/rushankshah65/'>
-                                            <i className="lni lni-linkedin-original lni-lg lni-25 social-media">LinkedIn</i>
-                                        </a>
-                                    </div>
-                                </Col>
-                                <Col md='2'>
-                                    <div>
-                                        <a target='_blank' style={{ color: 'black', textDecoration: 'none' }} href='https://www.instagram.com/rushankshah65/'>
-                                            <i className="lni lni-instagram lni-lg lni-25 social-media">Instagram</i>
-                                        </a>
-                                    </div>
-                                </Col>
-                                <Col md='2'>
-                                    <div>
-                                        <a target='_blank' style={{ color: 'black', textDecoration: 'none' }} href='https://github.com/rushankshah'>
-                                            <i className="lni lni-github-original lni-lg lni-25 social-media">GitHub</i>
-                                        </a>
-                                    </div>
-                                </Col>
-                                <Col md='2'>
-                                    <div>
-                                        <a target='_blank' style={{ color: 'black', textDecoration: 'none' }} href='https://twitter.com/ShahRushank'>
-                                            <i className="lni lni-twitter-original lni-lg lni-25 social-media">Twitter</i>
-                                        </a>
-                                    </div>
-                                </Col>
-                                <Col md='2'>
-                                    <div>
-                                        <a target='_blank' style={{ color: 'black', textDecoration: 'none' }} href='https://dev.to/rushankshah'>
-                                            <i className="lni lni-dev lni-lg lni-25 social-media">Dev</i>
-                                        </a>
-                                    </div>
-                                </Col>
-                                <Col md='2'>
-                                    <div>
-                                        <a target='_blank' style={{ color: 'black', textDecoration: 'none' }} href='https://rushankshah65.medium.com/'>
-                                            <i className="lni lni-medium lni-lg lni-25 social-media">Medium</i>
-                                        </a>
-                                    </div>
-                                </Col>
-                            </Row>
-                        </Col>
-                    </Row>
-                </Slide>
+                <Bounce>
+                    <div className='footer-social-icons'>
+                        <div>
+                            <a target='_blank' style={{ color: 'black', textDecoration: 'none' }} href='mailto:rushankshah65@gmail.com'>
+                                <img className='social-media' src='https://www.vectorlogo.zone/logos/gmail/gmail-icon.svg' />
+                            </a>
+                        </div>
+                        <div>
+                            <a target='_blank' style={{ color: 'black', textDecoration: 'none' }} href='https://www.linkedin.com/in/rushankshah65/'>
+                                <img className='social-media' src='https://www.vectorlogo.zone/logos/linkedin/linkedin-tile.svg' />
+                            </a>
+                        </div>
+                        <div>
+                            <a target='_blank' style={{ color: 'black', textDecoration: 'none' }} href='https://www.instagram.com/rushankshah65/'>
+                                <img className='social-media' src='https://www.vectorlogo.zone/logos/instagram/instagram-icon.svg' />
+                            </a>
+                        </div>
+                        <div>
+                            <a target='_blank' style={{ color: 'black', textDecoration: 'none' }} href='https://github.com/rushankshah'>
+                                <img className='social-media' src='https://www.vectorlogo.zone/logos/github/github-tile.svg' />
+                            </a>
+                        </div>
+                        <div>
+                            <a target='_blank' style={{ color: 'black', textDecoration: 'none' }} href='https://twitter.com/ShahRushank'>
+                                <img className='social-media' src='https://www.vectorlogo.zone/logos/twitter/twitter-icon.svg' />
+                            </a>
+                        </div>
+                        <div>
+                            <a target='_blank' style={{ color: 'black', textDecoration: 'none' }} href='https://rushankshah65.medium.com/'>
+                                <img className='social-media' src='https://www.vectorlogo.zone/logos/medium/medium-icon.svg' />
+                            </a>
+                        </div>
+                    </div>
+                </Bounce>
                 <Row className='mt-3'>
                 </Row>
             </div>
